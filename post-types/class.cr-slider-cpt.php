a5a0dc55ed5f96e71135e08094b3d50b84563d38
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-05-02 11:24:14 -0700

    Added validation so empty fields cannont be pushed to db

--- post-types/class.cr-slider-cpt.php
+++ post-types/class.cr-slider-cpt.php
@@ -72,9 +72,19 @@ if (!class_exists('CR_Slider_Post_Type')) {
                 $old_link_url = get_post_meta($post_id, 'url', true);
                 $new_link_url = $new_link_text = $_POST['cr_slider_link_url'];
 
-                //Sanatizes and pushes data to db
-                update_post_meta($post_id, 'cr_slider_text', sanitize_text_filed($new_link_text), $old_link_text);
-                update_post_meta($post_id, 'cr_slider_url', esc_url_raw($new_link_url), $old_link_url);
+                if (empty($new_link_text)) {
+                    update_post_meta($post_id, 'cr_slider_text', 'Add some text');
+                } else {
+                    //Sanatizes and pushes data to db
+                    update_post_meta($post_id, 'cr_slider_text', sanitize_text_filed($new_link_text), $old_link_text);
+                }
+
+                if (empty($new_link_url)) {
+                    update_post_meta($post_id, 'cr_slider_url', '#');
+                } else {
+                    //Sanatizes and pushes data to db
+                    update_post_meta($post_id, 'cr_slider_url', esc_url_raw($new_link_url), $old_link_url);
+                }
             }
         }
     }
