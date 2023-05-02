830f00483248114462a2bff0ed024181a0efa8b4
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-05-01 20:50:57 -0700

    text and url for the slides is now pushed to the postmeta table

--- post-types/class.cr-slider-cpt.php
+++ post-types/class.cr-slider-cpt.php
@@ -8,6 +8,7 @@ if (!class_exists('CR_Slider_Post_Type')) {
         {
             add_action('init', array($this, 'create_post_type'));
             add_action('add_meta_boxes', array($this, 'add_meta_boxes'));
+            add_action('save_post', array($this, 'save_post'), 10, 2);
         }
 
         public function create_post_type()
@@ -59,7 +60,21 @@ if (!class_exists('CR_Slider_Post_Type')) {
 
         public function add_inner_meta_boxes($post)
         {
-            # code...
+            //Gets the html
+            require_once CR_SLIDER_PATH . 'views/cr-slider_metabox.php';
+        }
+
+        public function save_post($post_id)
+        {
+            if (isset($_POST['action']) && $_POST['action'] == 'editpost') {
+                $old_link_text = get_post_meta($post_id, 'cr_slider_text', true);
+                $new_link_text = $_POST['cr_slider_link_text'];
+                $old_link_url = get_post_meta($post_id, 'url', true);
+                $new_link_url = $new_link_text = $_POST['cr_slider_link_url'];
+
+                update_post_meta($post_id, 'cr_slider_text', $new_link_text, $old_link_text);
+                update_post_meta($post_id, 'cr_slider_url', $new_link_url, $old_link_url);
+            }
         }
     }
 }
---
+++ views/cr-slider_metabox.php
@@ -0,0 +1,20 @@
+<table class="form-table mv-slider-metabox">
+    <tr>
+        <th>
+            <label for="cr_slider_link_text">Link Text</label>
+        </th>
+        <td>
+            <input type="text" name="cr_slider_link_text" id="cr_slider_link_text" class="regular-text link-text"
+                value="" required>
+        </td>
+    </tr>
+    <tr>
+        <th>
+            <label for="cr_slider_link_url">Link URL</label>
+        </th>
+        <td>
+            <input type="url" name="cr_slider_link_url" id="cr_slider_link_url" class="regular-text link-url" value=""
+                required>
+        </td>
+    </tr>
+</table>
