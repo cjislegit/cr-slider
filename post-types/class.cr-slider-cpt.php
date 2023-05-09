a1771d70758801c230b0806adc1fbf3d5e7f6f40
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-05-08 20:32:51 -0700

    Added columns to the slider table

--- post-types/class.cr-slider-cpt.php
+++ post-types/class.cr-slider-cpt.php
@@ -9,6 +9,8 @@ if (!class_exists('CR_Slider_Post_Type')) {
             add_action('init', array($this, 'create_post_type'));
             add_action('add_meta_boxes', array($this, 'add_meta_boxes'));
             add_action('save_post', array($this, 'save_post'), 10, 2);
+            //Adds columns to the slider table
+            add_filter('manage_cr-slider_posts_columns', array($this, 'cr_slider_cpt_columns'));
         }
 
         public function create_post_type()
@@ -46,6 +48,13 @@ if (!class_exists('CR_Slider_Post_Type')) {
             );
         }
 
+        public function cr_slider_cpt_columns($columns)
+        {
+            $columns['cr_slider_links_text'] = esc_html__('Link Text', 'cr-slider');
+            $columns['cr_slider_link_url'] = esc_html__('Link URL', 'cr-slider');
+            return $columns;
+        }
+
         public function add_meta_boxes()
         {
             add_meta_box(
@@ -66,24 +75,48 @@ if (!class_exists('CR_Slider_Post_Type')) {
 
         public function save_post($post_id)
         {
+            //Checks if nonce exists
+            if (isset($_POST['cr_slider_nonce'])) {
+                //Checks if the nonce is the correct one and if its not it ends the function
+                if (!wp_verify_nonce($_POST['cr_slider_nonce'], 'cr_slider_nonce')) {
+                    return;
+                }
+            }
+
+            // Checks if WP is autosaving and ends the function if it is
+            if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE) {
+                return;
+            }
+
+            //Checks if post type is set and if it is set to cr-slider
+            if (isset($_POST['post_type']) && $_POST['post_type'] === 'cr-slider') {
+                // Checks if the current user can edit pages  if they can't it ends the function
+                if (!current_user_can('edit_post', $post_id)) {
+                    return;
+                    //Checks if the user can edit post and if they can't it ends the function
+                } elseif (!current_user_can('edit_post', $post_id)) {
+                    return;
+                }
+            }
+
             if (isset($_POST['action']) && $_POST['action'] == 'editpost') {
-                $old_link_text = get_post_meta($post_id, 'cr_slider_text', true);
+                $old_link_text = get_post_meta($post_id, 'cr_slider_link_text', true);
                 $new_link_text = $_POST['cr_slider_link_text'];
-                $old_link_url = get_post_meta($post_id, 'url', true);
-                $new_link_url = $new_link_text = $_POST['cr_slider_link_url'];
+                $old_link_url = get_post_meta($post_id, 'cr_slider_link_url', true);
+                $new_link_url = $_POST['cr_slider_link_url'];
 
                 if (empty($new_link_text)) {
-                    update_post_meta($post_id, 'cr_slider_text', 'Add some text');
+                    update_post_meta($post_id, 'cr_slider_link_text', 'Add some text');
                 } else {
                     //Sanatizes and pushes data to db
-                    update_post_meta($post_id, 'cr_slider_text', sanitize_text_filed($new_link_text), $old_link_text);
+                    update_post_meta($post_id, 'cr_slider_link_text', sanitize_text_field($new_link_text), $old_link_text);
                 }
 
                 if (empty($new_link_url)) {
-                    update_post_meta($post_id, 'cr_slider_url', '#');
+                    update_post_meta($post_id, 'cr_slider_link_url', '#');
                 } else {
                     //Sanatizes and pushes data to db
-                    update_post_meta($post_id, 'cr_slider_url', esc_url_raw($new_link_url), $old_link_url);
+                    update_post_meta($post_id, 'cr_slider_link_url', sanitize_text_field($new_link_url), $old_link_url);
                 }
             }
         }
--- views/cr-slider_metabox.php
+++ views/cr-slider_metabox.php
@@ -1,11 +1,21 @@
-<table class="form-table mv-slider-metabox">
+<?php
+$meta = get_post_meta($post->ID);
+$link_text = get_post_meta($post->ID, 'cr_slider_link_text', true);
+$link_url = get_post_meta($post->ID, 'cr_slider_link_url', true);
+//var_dump( $link_text, $link_url );
+
+?>
+
+<table class="form-table cr-slider-metabox">
+    <!-- Creates a nonce for each slide -->
+    <input type="hidden" name="cr_slider_nonce" value="<?=wp_create_nonce("cr_slider_nonce")?>">
     <tr>
         <th>
             <label for="cr_slider_link_text">Link Text</label>
         </th>
         <td>
             <input type="text" name="cr_slider_link_text" id="cr_slider_link_text" class="regular-text link-text"
-                value="" required>
+                value="<?php echo (isset($link_text)) ? esc_html($link_text) : ''; ?>" required>
         </td>
     </tr>
     <tr>
@@ -13,8 +23,8 @@
             <label for="cr_slider_link_url">Link URL</label>
         </th>
         <td>
-            <input type="url" name="cr_slider_link_url" id="cr_slider_link_url" class="regular-text link-url" value=""
-                required>
+            <input type="url" name="cr_slider_link_url" id="cr_slider_link_url" class="regular-text link-url"
+                value="<?php echo (isset($link_url)) ? esc_url($link_url) : ''; ?>" required>
         </td>
     </tr>
 </table>
