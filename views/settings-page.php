0d53aa5222e7dd2fa564ddacf375a727e8c01519
Carlos Ramirez <carlosjramirez11@gmail.com>
2023-05-15 20:50:05 -0700

    Corrected the action name for the form to options

--- views/settings-page.php
+++ views/settings-page.php
@@ -1,6 +1,6 @@
 <div class="wrap">
     <h1><?=esc_html(get_admin_page_title());?></h1>
-    <form action="option.php" method="post">
+    <form action="options.php" method="post">
         <?php
 settings_fields('cr_slider_group');
 do_settings_sections('cr_slider_page1');
